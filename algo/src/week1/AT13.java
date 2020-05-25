62e978023736caf6c65869d15c67f0dc33a0b004
KJY <wpdud001@gmail.com>
2020-05-25 13:59:22 +0900

    AT13 Completed title : 자릿수 더하기 grade : A

---
+++ algo/src/week1/AT13.java
@@ -0,0 +1,35 @@
+package week1;
+
+import java.util.*;
+
+public class AT13 {
+
+	public static void main(String[] args) {
+		Solution13 s= new Solution13();
+		System.out.println(s.solution(123));
+
+		
+		
+		
+	}
+
+}
+ class Solution13{
+	 public int solution(int n) {
+        int answer = 0;
+        int i = 10;
+        if(n>9) {
+        	while((double)n/i>=0.1) {
+        		answer += ((double) n/i * i - n/i*i)/(i/10);
+        		n = n/i * i;
+        		i= i*10;
+        		System.out.println(answer);
+        	}
+        }else {
+        	answer = n;
+        }
+        return answer;
+    }
+ }
+
+ 
