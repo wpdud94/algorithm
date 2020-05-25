ddf3801b986a813ff3d51a884489663d7f7b43e6
KJY <wpdud001@gmail.com>
2020-05-25 14:00:09 +0900

    AT14 Completed title : 최소공약수와 최소공배수 grade : A

---
+++ algo/src/week1/AT14.java
@@ -0,0 +1,34 @@
+package week1;
+
+import java.util.*;
+
+public class AT14 {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		
+	}
+
+}
+ class Solution14{
+	 public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        //최대 공약수
+        int min = Math.min(n, m);
+        for(int i=min;i>0;i--) {
+        	if(n%i==0 && m%i==0) {
+        		answer[0]=i;
+        		break;
+        	}
+        }
+        //최소공배수
+        answer[1]= (n*m)/answer[0];
+        
+        return answer;
+	 }
+ }
+
+ 
