9716f093846061d6cae3f3ad212275330f9f49b9
KJY <wpdud001@gmail.com>
2020-05-25 13:55:58 +0900

    AT10 Completed title : 직사각형 별찍기 grade : A

---
+++ algo/src/week1/AT09_answer.java
@@ -0,0 +1,28 @@
+package week1;
+
+import java.util.Scanner;
+
+public class AT09_answer {
+
+	public static void main(String[] args) {
+		Solution09 s= new Solution09();
+		
+		
+	}
+
+}
+ class Solution09{
+	 public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        
+        for(int j=0;j<b;j++) {
+        for(int i=0;i<a;i++) System.out.print("*");
+        System.out.println("");
+        }
+        }
+ }
+
+ 
