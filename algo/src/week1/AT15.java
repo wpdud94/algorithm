e3b4f6e1f75cae9b7815b77dbf24caef47ad7e27
KJY <wpdud001@gmail.com>
2020-05-25 14:00:47 +0900

    AT15 Completed title : 정수 제곱근 판별 grade : B

---
+++ algo/src/week1/AT15.java
@@ -0,0 +1,29 @@
+package week1;
+
+import java.util.*;
+
+public class AT15 {
+
+	public static void main(String[] args) {
+		Solution15 s = new Solution15();
+		System.out.println(s.solution(121));
+		System.out.println(s.solution(11));
+
+		
+
+		
+	}
+
+}
+ class Solution15{
+	 public long solution(long n) {
+        long answer = -1;
+        double sqrt1 = Math.sqrt(n);
+        int sqrt2 = (int) Math.sqrt(n);
+        if(sqrt1==sqrt2)
+        answer = (long) Math.pow((long) Math.sqrt(n)+1, 2);
+        return answer;
+    }
+ }
+
+ 
