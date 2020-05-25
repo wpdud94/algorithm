817fbdf0251d5af50ab810883fdc85d1c82fcd2b
KJY <wpdud001@gmail.com>
2020-05-25 13:58:44 +0900

    AT12 Completed title : 짝수와 홀수 grade : A

---
+++ algo/src/week1/AT12.java
@@ -0,0 +1,29 @@
+package week1;
+
+import java.util.Scanner;
+
+public class AT12 {
+
+	public static void main(String[] args) {
+		Solution12 s= new Solution12();
+		System.out.println(s.solution(3));
+		System.out.println(s.solution(2));
+		
+		
+		
+	}
+
+}
+ class Solution12{
+	 public String solution(int num) {
+        String answer = "";
+        if(num%2==0) {
+        	answer="Even";
+        }else {
+        	answer="Odd";
+        }
+        return answer;
+    }
+ }
+
+ 
