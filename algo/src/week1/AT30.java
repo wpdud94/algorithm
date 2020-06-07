fcda5894b314f1d65d1cacd871d82866c508a378
kjy <wpdud001@gmail.com>
2020-06-07 23:33:58 +0900

    비밀지도 grade : fail

--- algo/src/week1/AT30.java
+++ algo/src/week1/AT30.java
@@ -6,18 +6,71 @@ public class AT30 {
 
 	public static void main(String[] args) {
 		Solution30 s = new Solution30();
+		int n = 6;
+		int[] arr1 = {46,33,33,22,31,50};
+		int[] arr2 = {27,56,19,14,14,10};
+		System.out.println(Arrays.toString(s.solution(n, arr1, arr2)));
 
 	}
 
 }
 class Solution30 {
 	public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
-        //1. ÀÌÁø¼ö·Î º¯È¯
-        Math.
-        //2. µÎ Áöµµ ÇÕÄ¡±â 0ÀÌ¿ÜÀÇ ¼ö´Â º®
+		String[] answer = new String[n];
+		for(int i=0;i<n;i++) {
+			String result = Integer.toBinaryString(arr1[i]|arr2[i]);
+			answer[i]=result.replaceAll("1","#").replaceAll("0", " ");
+		}
+/*        int[] temp = new int[n];
+        int idx=0;
+        //1. ì´ì§„ìˆ˜í™” + í•©ì¹˜ê¸°
+        for(int i=0;i<n;i++) {
+        	int two = 0;
+        	String str="";
+        	for(int j=n-1;j>-1;j--) {
+        		if(arr1[i]/Math.pow(2, j)>1) {
+        			arr1[i]=(int) (arr1[i]-Math.pow(2, j));
+        			two+=Math.pow(10, j);
+        		}else if(arr1[i]%Math.pow(2, j)==0.0) {
+        			two+=Math.pow(10, j);
+        			break;
+        		}else if(arr1[i]/Math.pow(2, j)>0) {
+        			continue;
+        		} 
+        	}
+        	for(int j=n-1;j>-1;j--) {
+        		if(arr2[i]/Math.pow(2, j)>1) {
+        			arr2[i]=(int) (arr2[i]-Math.pow(2, j));
+        			two+=Math.pow(10, j);
+        		}else if(arr2[i]%Math.pow(2, j)==0.0) {
+        			two+=Math.pow(10, j);
+        			break;
+        		}else if(arr2[i]/Math.pow(2, j)>0) {
+        			continue;
+        		}
+        	}
+        	temp[i]= two;
+        }
+        //System.out.println(Arrays.toString(temp));
+        //2. 0ì´ë©´ ê³µë°± ì•„ë‹ˆë©´ #
+        for(int i : temp) {
+        	String str = "";
+        	int t=n-1;
+        	while(t>-1) {
+        		if(i/Math.pow(10, t)>=1) {
+        			i=i-(int) ((int)(i/Math.pow(10, t))*Math.pow(10, t));
+        			str+="#";
+        		}else {
+        			str+=" ";
+        		}
+        		t--;
+        	}
+        	answer[idx]= str;
+        	idx++;
+        }
         
-        //3. #°ú °ø¹éÀ¸·Î ¹®ÀÚ¿­ ¹è¿­ Ãâ·Â
+       // System.out.println(Arrays.toString(answer));
+       */
         return answer;
     }
 }
