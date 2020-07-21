4bbf717477ddbb4ce744c1c167266571c87d0faa
kjy <wpdud001@gmail.com>
2020-07-21 16:27:08 +0900

    핵준특2_완전탐색>문자열압축(A)

--- algo/src/algostudy/AT02_search_strcompress.java
+++ algo/src/algostudy/AT02_search_strcompress.java
@@ -1,36 +1,54 @@
 package algostudy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class AT02_search_strcompress {
 	public static void main(String[] args) {
-		System.out.println("abc".substring(0, 1));
+		String s = "abcabcdede";
+		Solution02 sol = new Solution02();
+		System.out.println(sol.solution(s));
 	}
 }
 class Solution02 {
     public int solution(String s) {
         int answer = 0;
-        int minL = s.length();
-        int unit=0;
-        for(unit=0;unit<s.length();unit++) {
-        	
+        int unit =1;
+        int min = s.length();
+        while(unit<=s.length()) {
+        	LinkedList<String> que = new LinkedList<>();
+        	for(int i=0;i<s.length();i=i+unit) {
+        		try {
+        			que.add(s.substring(i, i+unit));
+        		}catch(StringIndexOutOfBoundsException e) {
+        			que.add(s.substring(i));
+        		}
+        	}
+        	//System.out.println(que);
+        	String result = "";
+        	int cnt=1;
+        	while(!que.isEmpty()) {
+        		String sout = que.pop();
+        		//System.out.println(sout);
+        		//System.out.println("peek : "+que.peek());
+        		if(sout.equals(que.peek())) {
+        			cnt++;
+        			continue;
+        		}else {
+        			if(cnt!=1) result += cnt+sout;
+        			else result += sout; 
+        			cnt=1;
+        			//System.out.println("result : "+result);
+        		}
+        	}
+        	//System.out.println("min : "+min);
+        	if(min>result.length()) min = result.length();
+        	unit++;
         }
+        answer = min;
         return answer;
-    }
-    
-    int searchSame(String s, int unit, int minL) {
-    	for(int i=0;i<s.length();i++) {
-    		if(s.substring(i,unit+i+1) != null);
-    	}
-    	
-    	
-    	
-		return unit;
     }
 }
 /*
-1. 단위 개수 1~length 하나하나 : 단위 수 == length?
-2. 단위 별로 연속으로 반복되는가?
-	인덱스 + 단위
-3. 압축한 문자열 길이 구하기
-4. min값 처리
 
 */
